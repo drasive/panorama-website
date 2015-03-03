3f9ecc87c7c90b74585f3cd8a68f63288b285a90
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2015-03-03 16:11:19 +0100

    Added a PanoramaLibrary and a PanoramaCreator project

---
+++ PanoramaService/PanoramaCreator/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PanoramaCreator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PanoramaCreator")]
+[assembly: AssemblyCopyright("Copyright © Dimitri Vranken 2015")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("657c1e07-ad21-41a7-a561-ebc8a006fda9")]
+
+[assembly: AssemblyVersion("1.0.0")]
