977a7d5f2bd0ef9623cac22969bbce43ace79394
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2015-03-19 16:18:32 +0100

    [Website] Added a favicon

--- Website/wwwroot/_footer.php
+++ Website/wwwroot/_footer.php
@@ -2,13 +2,18 @@
 
 <footer>
     <div class="row">
-        <div class="col-xs-4 col-xs-offset-2 text-center">
+        <div class="col-xs-4 text-center">
             <p>&copy; 2015 Dimitri Vranken</p>
         </div>
         <div class="col-xs-4 text-center">
             <p>
                 <a href="https://github.com/drasive/panorama-website" target="_blank">View on GitHub</a>
                 &nbsp;<a href="https://github.com/drasive/panorama-website" target="_blank"><img src="images/github_mark.png" height="22" /></a>
+            </p>
+        </div>
+        <div class="col-xs-4 text-center">
+            <p>
+                Icon made by <a href="http://www.icons8.com" title="Icons8">Icons8</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>
             </p>
         </div>
     </div>
